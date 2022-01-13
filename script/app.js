1dfc98df3748e04c5d0846b823ced29f47f07409
LenjaKoller <82869441+LenjaKoller@users.noreply.github.com>
2022-01-13 17:07:40 +0100

    produktinformation

---
+++ script/app.js
@@ -0,0 +1,112 @@
+/**
+ * Webapp with Pagination
+ */
+
+/**
+ * Deklaration der Variablen
+ */
+let index;
+let produkte = undefined;
+
+/**
+ * Ausgabe zeigen
+ */
+function showUI(){
+    //Identification
+    let htmlObj = document.getElementById("product");
+    htmlObj.innerHTML =
+        `Name: ${produkte.name}<br>`+
+        `Grössen: ${produkte.size}<br>`+
+        `Preis: ${produkte.prise}`;
+
+//Lehrbetrieb
+    htmlObj = document.getElementById("suited");
+    htmlObj.innerHTML =
+        `${produkte.suited}`;
+
+    //Kurse - Berufskunde
+    htmlObj = document.getElementById("skin");
+    htmlObj.innerHTML =
+        `${produkte.skin}`;
+
+    //Kurse - Berufskunde
+    htmlObj = document.getElementById("ingredients");
+    htmlObj.innerHTML =
+        `${produkte.ingredients}`;
+
+
+    //Index im Pagination-Element zeigen
+    htmlObj = document.getElementById("showIndex");
+    //clear
+    htmlObj.innerHTML = "";
+    //set
+    htmlObj.innerHTML = index;
+}
+
+
+/**
+ * Nächster Eintrag (Record) zeigen
+ */
+function showNext() {
+    //Index aus dem Browser-Speicher lesen
+    index = localStorage.getItem("index");
+    //Testausgabe auf der Console
+    console.log(index);
+    //Falls der index nicht definiert ist ...
+    if (index == undefined){
+        //... dann setze den index auf 0 (Anfang)
+        index = 0;
+    } else {
+        //... sonst falls der index eins kleiner als das Maximum ist
+        if (produkteListe.length-1 > index){
+            //... erhöhe den index um 1
+            index++;
+        }
+    }
+    //speichere wieder den index im Browser-Speicher
+    localStorage.setItem("index", index);
+    //hole den nächst höheren Eintrag
+    produkte = produkteListe[index];
+    //zeige den Eintrag
+    showUI();
+}
+
+/**
+ *  Vorhergehender Eintrag (Record) zeigen
+ */
+function showPrevious() {
+    //Index aus dem Browser-Speicher lesen
+    index = localStorage.getItem("index");
+    //Testausgabe auf der Console
+    console.log(index);
+    //Falls der index nicht definiert ist ...
+    if (index == undefined){
+        //... dann setze den index auf 0 (Anfang)
+        index = 0;
+    } else {
+        //... sonst falls der index grösser als das Minimum ist
+        if (index > 0){
+            //... vermindere den index um 1
+            index--;
+        }
+    }
+    //speichere wieder den index im Browser-Speicher
+    localStorage.setItem("index", index);
+    //hole den nächst höheren Eintrag
+    produkte = produkteListe[index];
+    //zeige den Eintrag
+    showUI();
+}
+
+//start app
+//Falls der index nicht definiert ist ...
+if (produkte === undefined){
+    //... dann setze den index auf 0 (Anfang)
+    index = 0;
+    //speichere wieder den index im Browser-Speicher
+    localStorage.setItem("index", index);
+    //hole den nächst höheren Eintrag
+    produkte = produkteListe[index];
+    //zeige den Eintrag
+    showUI();
+}
---
+++ script/data.js
@@ -0,0 +1,57 @@
+const professionList = ["Applikationsentwickler", "IMS",
+    "Mediamaitker", "Recyclist", "Detailhandel", "Kaufmann"];
+
+let produkteListe =
+    [
+        {
+            "name": "Amazing Face Cleanser",
+            "size": "1oo mL, 200 mL",
+            "prise": "33 CHF",
+            "suited": "Ölige Haut, Mischhaut und großporige Haut, Sommer, warme und feuchte klimatische Bedingungen",
+            "skin": "Erfrischt, ausgeglichen und makellos rein",
+            "ingredients": "Mandarinenschale, Ylang-Ylang, Lavendelöl"
+        },
+        {
+            "name": "Chamomile Concentrate Anti-Blemish Masque",
+            "size": "60 mL",
+            "prise": "43 CHF",
+            "suited": "Unreine, gereizte Haut",
+            "skin": "Geklärt, beruhigt und tiefenwirksam gereinigt",
+            "ingredients": "Kamillenknospe, Teebaumblatt, Nachtkerze"
+        },
+        {
+            "name": "Parsley Seed Anti-Oxidant Serum",
+            "size": "100 mL",
+            "prise": "73 CHF",
+            "suited": "Normale und empfindliche Haut sowie Mischhaut",
+            "skin": "Weich und hydratisiert, mit einem mattierten Finish",
+            "ingredients": "Traubenkern, Panthenol, Petersiliensamen"
+        },
+
+
+
+        {
+            "name": "Colour Protection Shampoo",
+            "size": "200 mL",
+            "prise": "25 CHF",
+            "suited": "Vom Friseur behandeltes, koloriertes Haar",
+            "skin": "Minzig, nach Kräutern duftend, nussig",
+            "ingredients": "Mandarinenschale, Panthenol, hydrolysierter Hafer"
+        },
+        {
+            "name": "Equalising Shampoo",
+            "size": "500 mL",
+            "prise": "49 CHF",
+            "suited": "Fettende Haare, Menschen mit einem aktiven Lebensstil und Menschen, die in einer verschmutzten städtischen Umgebung leben",
+            "skin": "Aromatisch-holzig, nach Kräutern duftend, frisch",
+            "ingredients": "Fenchel, Eukalyptusblatt, Mistelblatt"
+        },
+        {
+            "name": "Sage & Cedar Scalp Treatment",
+            "size": "25 mL",
+            "prise": "33 CHF",
+            "suited": "Reaktive, trockene und gestresste Kopfhaut",
+            "skin": "Krautig, Holzig",
+            "ingredients": "Rosmarinblatt, Atlaszeder, Salbeiblatt"
+        }
+    ]
