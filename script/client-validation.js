7b8e50b61d304d9970e55dd4a04eca627603b6b1
LenjaKoller <82869441+LenjaKoller@users.noreply.github.com>
2022-06-05 20:07:52 +0200

    Fehlermeldungen und Styles überarbeitet

---
+++ script/client-validation.js
@@ -0,0 +1,84 @@
+// read form element
+const form = document.getElementById('form');
+const lastname = document.getElementById('name');
+const firstname = document.getElementById('vorname');
+const email = document.getElementById('email');
+const password = document.getElementById('passwort');
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+// Check email is valid
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email ist nicht verfügbar');
+  }
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+  let isRequired = false;
+  inputArr.forEach(function(input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+      isRequired = true;
+    } else {
+      showSuccess(input);
+    }
+  });
+
+  return isRequired;
+}
+
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(input,
+        `${getFieldName(input)} muss mindestens ${min} Zeichen enthalten`
+    );
+  } else if (input.value.length > max) {
+    showError(input,
+        `${getFieldName(input)} darf maximal ${max} Zeichen enthalten`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+// Get fieldname
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// Validate form input elements
+function validateForm(){
+  if(!checkRequired([lastname, firstname, email, password])){
+    checkLength(lastname, 3, 15);
+    checkLength(firstname, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+  }
+}
+
+
+// Event listeners
+form.addEventListener('submit', function(e) {
+  //https://www.w3schools.com/jsref/event_preventdefault.asp
+  e.preventDefault();
+  //First validate form
+  validateForm();
+});
