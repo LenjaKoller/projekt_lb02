d6387af9b05161b525abf8e4e284d6d4216dd031
LenjaKoller <82869441+LenjaKoller@users.noreply.github.com>
2022-06-20 23:01:16 +0200

    Telefonnummer validiert

--- client/script/client-validation.js
+++ client/script/client-validation.js
@@ -30,7 +30,7 @@ function checkEmail(input) {
   }
 }
 
-// Check email is valid
+// Check number is valid
 function checkNumber(input) {
   const re = /(\b(0041|0)|\B\+41)(\s?\(0\))?(\s)?[1-9]{2}(\s)?[0-9]{3}(\s)?[0-9]{2}(\s)?[0-9]{2}\b/
   if (re.test(input.value.trim())) {
